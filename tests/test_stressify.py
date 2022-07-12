aa8a668fd8503826210501ef3c2aef26016e85d1
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2022-07-12 14:16:28 +0300

    Find stress in non-ambigous cases

--- tests/test_stressify.py
+++ tests/test_stressify.py
@@ -1,4 +1,7 @@
-from ukrainian_stress import stressify
+from ukrainian_stress import stressify, get_accent_positions, ACCENT
+import marisa_trie
+import pytest
+
 
 
 def test_single_syllable():
@@ -7,4 +10,32 @@ def test_single_syllable():
 
 
 def test_single_choice():
-    assert stressify("Україна") == "Украї'на"
+    assert stressify("Україна") == f"Украї{ACCENT}на"
+
+
+def test_get_accent_positions(trie):
+    parse = {
+        "id": 1,
+        "text": "Україна",
+        "lemma": "Україна",
+        "upos": "PROPN",
+        "xpos": "Npfsnn",
+        "feats": "Animacy=Inan|Case=Nom|Gender=Fem|Number=Sing",
+        "head": 0,
+        "deprel": "root",
+        "start_char": 0,
+        "end_char": 7,
+        "ner": "S-LOC",
+        "multi_ner": [
+            "S-LOC"
+        ]
+    }
+    expected = [5]
+    assert get_accent_positions(trie, parse) == expected
+
+
+@pytest.fixture(scope='module')
+def trie():
+    result = marisa_trie.BytesTrie()
+    result.load("./stress.v1.trie")
+    return result
--- ukrainian_stress/stressify.py
+++ ukrainian_stress/stressify.py
@@ -1,2 +1,76 @@
+import marisa_trie
+
+
+ACCENT = '\u0301'
+
+
+class Stressify:
+
+    def __init__(self, dict_path="./stress.v1.trie"):
+        import stanza
+        self.dict = marisa_trie.BytesTrie()
+        self.dict.load(dict_path)
+        self.nlp = stanza.Pipeline('uk', 'tokenize,pos')
+
+    def __call__(self, text):
+        parsed = self.nlp(text)
+        result = []
+        for token in parsed.iter_tokens():
+            accents = get_accent_positions(self.dict, token.to_dict()[0])
+            result.append(apply_accent_positions(token.text, accents))
+        return " ".join(result)  # restore original whitespace
+
+
+
 def stressify(text: str) -> str:
-    return text
+    if not hasattr(stressify, "f"):
+        stressify.f = Stressify()
+    return stressify.f(text)
+
+
+def get_accent_positions(trie, parse):
+    base = parse['text']
+    if base not in trie:
+        return []
+
+    values = trie[base]
+    assert len(values) == 1
+
+    accents_by_tags = _parse_value(values[0])
+
+    if len(accents_by_tags) == 1:
+        return accents_by_tags[0][1]
+
+    for tags, accents in accents_by_tags.items():
+        if f'upos={parse["upos"]}' not in tags:
+            continue
+        return accents
+
+    return []
+
+
+def _parse_value(value):
+    accents_by_tags = []
+    
+    if b'$' not in value:
+        # single item
+        accents = [int(b) for b in value]
+        tags = []
+        accents_by_tags.append((tags, accents))
+
+    else:
+        items = value.split(b'$')
+        for item in items:
+            accents, _, tags = item.split(b'^')
+            accents = [int.from_bytes(b, 'little') for b in accents]
+            tags = tags.decode()
+            accents_by_tags.append((tags, accents))
+
+    return accents_by_tags
+
+
+def apply_accent_positions(s, positions):
+    for position in sorted(positions, reverse=True):
+        s = s[:position] + ACCENT + s[position:]
+    return s
+
